d39c688f614ab80a499048b12183f8b7b910282f
Aditya Bhattacharjee <adityab76@gmail.com>
2023-07-26 23:27:55 +0530

    newques

--- min-stack.cpp
+++ min-stack.cpp
@@ -1,8 +1,49 @@
-//leetcode - medium 
-
-#include<iostream>
-using namespace std;
-int main()
-{
-return 0;
+class MinStack {
+public:
+stack<int> st , min_stack ; 
+ 
+    MinStack() {
+       
+        
+    }
+    
+   void push(int val) {
+       st.push(val) ; 
+       if(min_stack.empty() || val<= min_stack.top())
+       {
+           min_stack.push(val) ; 
+       }
+    
 }
+
+    
+    void pop() {
+        if(st.top()==min_stack.top())
+        {
+            min_stack.pop() ; 
+        }
+        st.pop();
+
+       
+    }
+    
+    int top() {
+        return st.top() ;
+
+       
+    }
+     int getMin() {
+         return min_stack.top() ; 
+        
+    }
+    
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
