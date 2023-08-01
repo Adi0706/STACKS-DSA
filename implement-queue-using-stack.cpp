4924d036730b5fb9990e564aa5c7ab7020be77d4
Aditya Bhattacharjee <adityab76@gmail.com>
2023-08-01 23:54:06 +0530

    new questions

--- implement-queue-using-stack.cpp
+++ implement-queue-using-stack.cpp
@@ -4,35 +4,36 @@
 
 class MyQueue {
 public:
-stack<int> input,output ;
+stack<int> input,output ; // 2 stacks 
     MyQueue() {
         
     }
     
     void push(int x) {
-        input.push(x) ; 
+        input.push(x) ; // push all the elements in input stack
         
     }
     
     int pop() {
-        int val = peek() ; 
-        output.pop() ; 
+        int val = peek() ; // as per queue--> top element of output stack will be front element of queue and first element of input stack
+        output.pop() ; // popping the top element and return ing it 
         return val ; 
         
     }
     
     int peek() {
-        if(output.empty()){
-            while(!input.empty()){
+
+        if(output.empty()){//if the output stack is empty
+            while(!input.empty()){ // push all the input elements in the output elements while the input is not getting empty
                 output.push(input.top());
                 input.pop();
             }
         }
-        return output.top() ; 
+        return output.top() ; //top element of the output stack
     }
     
     bool empty() {
-        return input.empty() && output.empty() ; 
+        return input.empty() && output.empty() ; // return if input and output are empty--> means the queue is empty.
         
     }
 };
